021e4641afa81f6ed2b535081310f57199d4e4e0
gokarnkark07 <gokarnkark07@gmail.com>
2024-10-16 16:26:26 +0000

    added domain in the syndicate form , add and view profile data made same and then   -- schema chnage required @ shakthi

--- public/js/syndicate_client_data_entry.js
+++ public/js/syndicate_client_data_entry.js
@@ -54,6 +54,7 @@ function displayClientDetails(client) {
                 <div>
                     <p class="text-lg font-bold">Name: ${client.name || 'N/A'}</p>
                     <p class="text-sm">Company: ${client.companyName || 'N/A'}</p>
+                    <p class="text-sm">Domain: ${client.domainName || 'N/A'}</p>
                     <p class="text-sm">Person To Meet: ${client.personToMeet || 'N/A'}</p>
                     <p class="text-sm">Referred By: ${client.personReferred || 'N/A'}</p>
                     <p class="text-sm">Email: ${client.email || 'N/A'}</p>
